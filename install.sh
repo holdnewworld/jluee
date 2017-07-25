c5e29f5c52804441c81988980fabab302f698af7
labrador <273601727@qq.com>
2017-07-25 15:51:15 +0800

    correct links

--- install.sh
+++ install.sh
@@ -137,5 +137,5 @@ sed -i "s/sspanelv2/mudbjson/g" /usr/local/shadowsocksr/userapiconfig.py
 sed -i "s/UPDATE_TIME = 60/UPDATE_TIME = 10/g" /usr/local/shadowsocksr/userapiconfig.py
 sed -i "s/SERVER_PUB_ADDR = '127.0.0.1'/SERVER_PUB_ADDR = '$(wget -qO- -t1 -T2 ipinfo.io/ip)'/" /usr/local/shadowsocksr/userapiconfig.py
 #INstall Success
-bash /usr/local/SSR-Bash-Python/self-check.sh
+bash /usr/local/jluee/self-check.sh
 echo 'enjoy it!'
