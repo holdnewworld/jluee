50000568268f29d0a3bb2e2375eb0ec270c494b1
labrador <273601727@qq.com>
2017-07-30 13:05:48 +0800

    修改自动重启

---
+++ autoreboot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+#change time zone to shanghai
+rm -rf /etc/localtime
+ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+ntpdate us.pool.ntp.org
+echo `date`
+echo '切换时区为shanghai'
+echo ''
+
+#add autoreboot at 02:00 everyday
+echo '07 13  * * * root /sbin/reboot'
+service cron restart
+echo "设置服务器在每天凌晨2点重启"
+echo ''
