e9890333c8954c3b732a07fa1c97f74900bb55c9
labrador <273601727@qq.com>
2017-07-30 18:20:19 +0800

    修改代码规范

--- usergenerate.py
+++ usergenerate.py
@@ -1,29 +1,3 @@
-#python mujson_mgr.py -a -u 11 -p 4167 -k RqobVTAh -m rc4-md5 -O origin -o plain -t 50
-
-'''
-import subprocess 
-#handle = open('../tmp.log', 'a')
-subprocess.Popen(['ipconfig', '-all'], stdout='a.txt')
-'''
-
-'''
-import subprocess 
-output = subprocess.Popen(['ifconfig', '-all'], stdout=subprocess.PIPE, shell=True)
-oc = output.communicate()
-
-for i in oc:
-	print i
-
-print len(oc)
-'''
-
-'''
-import subprocess 
-p = subprocess.Popen('ls -all', stdout=subprocess.PIPE,shell=True)
-for i in p.stdout.readlines():
-	print i
-'''
-
 import random
 import string
 import subprocess 
@@ -48,7 +22,7 @@ def main():
 	cmd = 'python mujson_mgr.py -a -u ' + str(user) + ' -p ' + str(port) + ' -k ' + str(passwd) + ' -m rc4-md5 -O origin -o http_simple -t 50'
 	print cmd
 	p = subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True)
-	#python mujson_mgr.py -a -u 11 -p 4167 -k RqobVTAh -m rc4-md5 -O origin -o plain -t 50
+	
 	for i in p.stdout.readlines():
 		print i
 		Message.append(i)
@@ -67,7 +41,7 @@ def main():
 	p = subprocess.Popen(iptables, stdout=subprocess.PIPE,shell=True)
 	iptables = 'iptables -I INPUT -m state --state NEW -m udp -p udp --dport ' + str(port) + ' -j ACCEPT'
 	p = subprocess.Popen(iptables, stdout=subprocess.PIPE,shell=True)
-#['python mujson_mgr.py -a -u ',str(user), ' -p ',str(port), ' -k ',str(passwd),' -m rc4-md5 -O origin -o plain -t 50']
+
 
 for i in range(1,20):
 	main()
